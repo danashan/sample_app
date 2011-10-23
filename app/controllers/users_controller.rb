9490cee2f36f755c11294889beace9c3bee459f3
Dana <danashan@gmail.com>
2011-10-23 09:10:19 -0700

    fixing heroku deployment errors 3.1 via heroku logs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
+
   
   def show
     @user = User.find(params[:id])
   end
-=======
->>>>>>> 214cc4f6c49b2b2a2b5ce2aaa986f67f8930a8c0
+
   def new
     @title = "Sign up"
   end
